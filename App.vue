e9b1e51b6ea4f0daccdc97d9a43c2be2612754c8
Andre Soares Gomes <54090940+AndreLotusDev@users.noreply.github.com>
2020-09-11 15:38:54 -0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+      <hr>
+      <app-filmes filmeTitulo="Homem de ferro"/>
+      <app-filmes filmeTitulo="Homem de ferro"/>
+      <app-filmes filmeTitulo="Homem de ferro"/>
+      <app-filmes filmeTitulo="Homem de ferro"/>
+  </div>
+</template>
+
+<script>
+import Filmes from './Filmes.vue'
+
+export default {
+  components: {'app-filmes': Filmes},
+}
+</script>
+
+<style>
+
+</style>
---
+++ Filmes.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <hr>
+      <h1>{{atualizar()}}</h1>
+      <hr>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['filmeTitulo'],
+
+    methods:{
+        atualizar()
+        {
+            return this.filmeTitulo += " - 1"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
